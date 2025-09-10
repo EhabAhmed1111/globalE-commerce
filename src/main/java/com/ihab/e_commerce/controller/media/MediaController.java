d8b0ecf567112f41dc0c591e3c7f4e3d6529d7c6
Ehab Ahmed <ehab.ahmed0202@gmail.com>
2025-09-10 19:16:46 +0300

    product controller almost completed

---
+++ src/main/java/com/ihab/e_commerce/controller/media/MediaController.java
@@ -0,0 +1,14 @@
+package com.ihab.e_commerce.controller.media;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("${api.prefix}/medias")
+public class MediaController {
+}
---
+++ src/test/java/com/ihab/e_commerce/service/product/ProductServiceTest.java
@@ -0,0 +1,11 @@
+package com.ihab.e_commerce.service.product;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceTest {
+
+}
